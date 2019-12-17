1f339df282fbe2a15276abbcf3bae9e43bed0264
mpasilan <pmichaeljay@gmail.com>
2019-12-17 17:36:15 +0800

    integrate admin template

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin_layout')
+@section('content')
+<!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="#" ><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+          </div>
+
+          <!-- Content Row -->
+          <div class="row">
+
+           content area!
+           
+        </div>
+        <!-- /.container-fluid -->
+@endsection
