1983786cf8cf785602e1a282aa436e2174d0fb2e
robiepamisa <robiejoshpamisa@gmail.com>
2019-12-18 18:29:40 +0800

    Please fix

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -19,21 +19,19 @@
         <thead>
           <tr>
             <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Age</th>
-            <th>Start date</th>
-            <th>Salary</th>
+            <th>Ticket</th>
+            <th>Assigned to</th>
+            <th>Status</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tfoot>
           <tr>
             <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Age</th>
-            <th>Start date</th>
-            <th>Salary</th>
+            <th>Ticket</th>
+            <th>Assigned to</th>
+            <th>Status</th>
+            <th>Action</th>
           </tr>
         </tfoot>
         <tbody>
@@ -41,121 +39,121 @@
             <td>Tiger Nixon</td>
             <td>System Architect</td>
             <td>Edinburgh</td>
-            <td>61</td>
-            <td>2011/04/25</td>
-            <td>$320,800</td>
+            <td>
+                Pending 
+            </td>
+            <td>
+          
+                  <div class="dropdown no-arrow mb-4 ">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Dropdown (no arrow)
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div>
+                
+            </td>
           </tr>
           <tr>
             <td>Garrett Winters</td>
             <td>Accountant</td>
             <td>Tokyo</td>
             <td>63</td>
-            <td>2011/07/25</td>
-            <td>$170,750</td>
+            
           </tr>
           <tr>
             <td>Ashton Cox</td>
             <td>Junior Technical Author</td>
             <td>San Francisco</td>
             <td>66</td>
-            <td>2009/01/12</td>
-            <td>$86,000</td>
+            
           </tr>
           <tr>
             <td>Cedric Kelly</td>
             <td>Senior Javascript Developer</td>
             <td>Edinburgh</td>
             <td>22</td>
-            <td>2012/03/29</td>
-            <td>$433,060</td>
+            
           </tr>
           <tr>
             <td>Airi Satou</td>
             <td>Accountant</td>
             <td>Tokyo</td>
             <td>33</td>
-            <td>2008/11/28</td>
-            <td>$162,700</td>
+            
           </tr>
           <tr>
             <td>Brielle Williamson</td>
             <td>Integration Specialist</td>
             <td>New York</td>
             <td>61</td>
-            <td>2012/12/02</td>
-            <td>$372,000</td>
+            
           </tr>
           <tr>
             <td>Herrod Chandler</td>
             <td>Sales Assistant</td>
             <td>San Francisco</td>
             <td>59</td>
-            <td>2012/08/06</td>
-            <td>$137,500</td>
+            
           </tr>
           <tr>
             <td>Rhona Davidson</td>
             <td>Integration Specialist</td>
             <td>Tokyo</td>
             <td>55</td>
-            <td>2010/10/14</td>
-            <td>$327,900</td>
+            
           </tr>
           <tr>
             <td>Colleen Hurst</td>
             <td>Javascript Developer</td>
             <td>San Francisco</td>
             <td>39</td>
-            <td>2009/09/15</td>
-            <td>$205,500</td>
+            
           </tr>
           <tr>
             <td>Lael Greer</td>
             <td>Systems Administrator</td>
             <td>London</td>
             <td>21</td>
-            <td>2009/02/27</td>
-            <td>$103,500</td>
+            
           </tr>
           <tr>
             <td>Jonas Alexander</td>
             <td>Developer</td>
             <td>San Francisco</td>
             <td>30</td>
-            <td>2010/07/14</td>
-            <td>$86,500</td>
+            
           </tr>
           <tr>
             <td>Shad Decker</td>
             <td>Regional Director</td>
             <td>Edinburgh</td>
             <td>51</td>
-            <td>2008/11/13</td>
-            <td>$183,000</td>
+            
           </tr>
           <tr>
             <td>Michael Bruce</td>
             <td>Javascript Developer</td>
             <td>Singapore</td>
             <td>29</td>
-            <td>2011/06/27</td>
-            <td>$183,000</td>
+            
           </tr>
           <tr>
             <td>Donna Snider</td>
             <td>Customer Support</td>
             <td>New York</td>
             <td>27</td>
-            <td>2011/01/25</td>
-            <td>$112,000</td>
+            
           </tr>
           <tr>
             <td>Robie Josh Pamisa</td>
             <td>Admin Philippines</td>
             <td>Gusa</td>
             <td>20</td>
-            <td>2011/01/25</td>
-            <td>$10,000,000</td>
+            
           </tr>
         </tbody>
       </table>
