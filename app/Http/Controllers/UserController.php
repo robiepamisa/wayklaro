c8280101be948377c9d54362403cab2857fb5770
robiepamisa <robiejoshpamisa@gmail.com>
2019-12-19 12:41:12 +0800

    added login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+       
+        $checkLogin = DB::table('users')->where(["email"=>$request['email'],"password"=>$request['password']])->get();
+        if(count($checkLogin) > 0)
+        {
+            echo"Login successful";
+        }
+        else
+        {
+            echo"Login failed wrong data passed";
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Auth::routes();
 
 Route::get('/admin', 'AdminController@index')->name('dashboard');
 Route::get('/create_user', 'AdminController@create_user')->name('create_user');
+Route::POST('/loginme', 'UserController@login');
+Route::get("/login",function(){
+    return view('login');
+});
