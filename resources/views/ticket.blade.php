2eed779808b0844ce6ce0eff436c07e14be20e5c
mpasilan <pmichaeljay@gmail.com>
2020-01-06 17:39:42 +0800

    ticket design

--- resources/views/ticket.blade.php
+++ resources/views/ticket.blade.php
@@ -4,20 +4,21 @@
         
 <section>
         <div class="container-fluid">
-        <div class="d-sm-flex align-items-center justify-content-between mb-0">
-            <h1
-                class="h1 mb-8 text-gray-800 grid">{{$ticket->subject}}
-            </h1>
-        </div>
-            <div class="row">
-                <div class="col-8 display-4">
-                <strong>{{$ticket->description}}</strong>
-                </div>
-                
-                <div>
-                asdasd
-                </div>
-            </div>
+<div class="jumbotron">
+            <h2 class="blog-post-title">Another blog post</h2>
+            <p class="lead">December 23, 2013 by <a href="#">Jacob</a></p>
+
+            <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+            <blockquote>
+              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </blockquote>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+          </div>
         </div>
 </section>
+
+            
+
+
 @endsection
